6d41468e63ef84f166046cd5fc0b4fd6b80c4a03
Craig McAlister <craigmack704@gmail.com>
2019-07-01 09:02:25 -0400

    added buttons in html and edited more javascript'

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,6 @@
 //-----------------------------
 
-var numbers = [30,31,32,33,34,35,36,37,38,39];
+var numbers = ["30", "31", "32", "33", "34","35", "36", "37", "38", "39"]
 var wins = 0;
 var losses = 0;
 var score = 0;
@@ -8,5 +8,26 @@ var winsText = document.getElementById("wins");
 var lossesText = document.getElementById("losses");
 var scoreText = document.getElementById("score");
 var randomNumberText = document.getElementById("random-number");
-var randomNumber = numbers[Math.floor(Math.random() * numbers.length)];
+var randomNumber = Math.floor(Math.random() * 39) + 1;
+var crystalValue = Math.floor(Math.random() * 9) + 1;
+
+$("#pc-num").text(randomNumber);
+console.log(randomNumber);
+
+
+$(document).ready(function(){
+    
+
+    if  (score === randomNumber) {
+        wins++;
+
+    } else if (score >= randomNumber) {
+        losses++;
+    } else {
+
+    }
+
+
+
+});
 
